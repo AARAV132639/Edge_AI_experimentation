6efdcb717f885398959d94022311a7c07b6782f1
AARAV132639 <aaravkumar2809@gmail.com>
2026-06-03 22:29:18 +0530

    version 1: camera accces done

---
+++ Object_detector_mob/app/src/main/java/com/example/edge_ai_phase_obj_detector_mob_ver/MainActivity.kt
@@ -0,0 +1,135 @@
+package com.example.edge_ai_phase_obj_detector_mob_ver
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.edge_ai_phase_obj_detector_mob_ver.ui.theme.Edge_ai_phase_obj_detector_mob_verTheme
+
+//adding camera imports
+import androidx.camera.view.PreviewView
+import androidx.compose.ui.viewinterop.AndroidView
+
+import androidx.camera.core.Preview.Builder
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.core.CameraSelector
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.compose.LocalLifecycleOwner
+
+//phase 2: imports---> enabling front camera
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CameraScreen()
+                }
+            }
+        }
+
+
+@Composable
+fun CameraScreen()
+{
+   var isFrontCamera by remember{mutableStateOf(false)}
+    key(isFrontCamera){
+        CameraPreview(isFrontCamera=isFrontCamera)
+    }
+
+    Box(modifier= Modifier.fillMaxSize())
+    {
+        CameraPreview(isFrontCamera=isFrontCamera)
+
+        Button(
+            onClick={isFrontCamera= !isFrontCamera},
+            modifier= Modifier.align(Alignment.BottomCenter)
+                .padding(16.dp)
+        )
+        {
+            Text(
+                if(isFrontCamera) "Front camera"
+                else "Back camera"
+            )
+        }
+    }
+
+}
+
+// New function for camera working
+@Composable
+fun CameraPreview( isFrontCamera: Boolean)
+{
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    AndroidView(
+        factory={ context-> val previewView= PreviewView(context)
+            val cameraProviderFuture= ProcessCameraProvider.getInstance(context)
+
+            cameraProviderFuture.addListener(
+                {
+                    val cameraProvider = cameraProviderFuture.get()
+                    val preview = androidx.camera.core.Preview.Builder().build()
+
+                    preview.surfaceProvider = previewView.surfaceProvider
+
+                   //adding switch
+                    val cameraSelector=
+                        if(isFrontCamera) CameraSelector.DEFAULT_FRONT_CAMERA
+                    else CameraSelector.DEFAULT_BACK_CAMERA
+
+                    try {
+                        cameraProvider.unbindAll()
+
+                        cameraProvider.bindToLifecycle(
+                            lifecycleOwner,
+                            cameraSelector,
+                            preview
+                        )
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                },
+                ContextCompat.getMainExecutor((context))
+
+            )
+            previewView
+        }
+    )
+}
+
+
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Edge_ai_phase_obj_detector_mob_verTheme {
+        Greeting("Android")
+    }
+}
---
+++ Object_detector_mob/settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "edge_ai_phase_obj_detector_mob_ver"
+include(":app")
+ 
