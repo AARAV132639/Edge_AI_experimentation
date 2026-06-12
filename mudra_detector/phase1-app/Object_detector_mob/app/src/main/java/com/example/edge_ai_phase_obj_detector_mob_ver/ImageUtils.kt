b23758bc7e8da8745b09625aef99e369288f6f10
AARAV132639 <aaravkumar2809@gmail.com>
2026-06-12 17:40:24 +0530

    bit map conversion

---
+++ mudra_detector/phase1-app/Object_detector_mob/app/src/main/java/com/example/edge_ai_phase_obj_detector_mob_ver/ImageUtils.kt
@@ -0,0 +1,51 @@
+package com.example.edge_ai_phase_obj_detector_mob_ver
+import android.graphics.Bitmap
+import android.graphics.ImageFormat
+import android.graphics.YuvImage
+import androidx.camera.core.ImageProxy
+import java.io.ByteArrayOutputStream
+
+fun ImageProxy.toBitmap(): Bitmap{
+    val yBuffer = planes[0].buffer
+    val uBuffer= planes[1].buffer
+    val vBuffer = planes[2].buffer
+
+    val ySize= yBuffer.remaining()
+    val uSize= uBuffer.remaining()
+    val vSize= vBuffer.remaining()
+
+    val nv21 = ByteArray(ySize+ uSize+vSize)
+    yBuffer.get(nv21,0,ySize)
+    vBuffer.get(nv21, ySize, vSize)
+    vBuffer.get(nv21, ySize+vSize,uSize)
+
+    val yuvImage= YuvImage(
+        nv21,
+        ImageFormat.NV21,
+        width,
+        height,
+        null
+    )
+
+    val out= ByteArrayOutputStream()
+
+    yuvImage.compressToJpeg(
+        android.graphics.Rect(
+            0,
+            0,
+            width,
+            height
+        ),
+        100,
+        out
+    )
+
+    val imageBytes= out.toByteArray()
+
+    return android.graphics.BitmapFactory.decodeByteArray(
+        imageBytes,
+        0,
+        imageBytes.size
+    )
+
+}
